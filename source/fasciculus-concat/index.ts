f478a66afc56d84949bac71c37dd06a341522bc3
rhjoerg <rhjoerg@gmail.com>
2024-02-12 18:51:57 +0100

    hunting imports

--- source/fasciculus-concat/index.ts
+++ source/fasciculus-concat/index.ts
@@ -30,9 +30,10 @@ class Config
 
 class TSConfig
 {
-    readonly file: string;
-    readonly parsed: TS.ParsedCommandLine;
+    private readonly file: string;
 
+    readonly options: TS.CompilerOptions;
+    readonly root: string;
     readonly files: string[];
 
     constructor()
@@ -41,11 +42,26 @@ class TSConfig
 
         if (!file) throw new Error("no tsconfig.json");
 
-        const content = TS.readConfigFile(file, TS.sys.readFile);
-
         this.file = file;
-        this.parsed = TS.parseJsonConfigFileContent(content, TS.sys, "./");
-        this.files = this.parsed.fileNames;
+
+        const content = TS.readConfigFile(file, TS.sys.readFile);
+        const config: any | undefined = content.config;
+        const options: any = config["compilerOptions"];
+
+        if (!config) throw new Error("no config");
+        if (!options["rootDir"]) throw new Error("no rootDir in tsconfig.json");
+
+        var root: string = unixify(options["rootDir"]);
+
+        if (root.startsWith("./")) root = root.substring(2);
+        if (root.endsWith("/")) root = root.substring(0, root.length - 1);
+
+        this.root = root;
+
+        const parsed: TS.ParsedCommandLine = TS.parseJsonConfigFileContent(content, TS.sys, "./");
+
+        this.options = parsed.options;
+        this.files = parsed.fileNames;
     }
 
     convert(main: string)
@@ -68,20 +84,74 @@ class TSConfig
     }
 }
 
+class Keys
+{
+    static createKey(root: string, file: string): string
+    {
+        var result: string = unixify(Path.relative(root, file));
+
+        if (result.endsWith(".ts")) result = result.substring(0, result.length - 3);
+
+        return result;
+    }
+}
+
+class Source
+{
+    readonly file: string;
+    readonly key: string;
+    readonly source: TS.SourceFile;
+
+    constructor(file: string, config: TSConfig)
+    {
+        this.file = file;
+        this.key = Keys.createKey(config.root, file);
+
+        const program: TS.Program = TS.createProgram([file], config.options);
+        const source: TS.SourceFile | undefined = program.getSourceFile(file);
+
+        if (!source) throw new Error("no source");
+
+        this.source = source;
+    }
+}
+
+class Sources
+{
+    readonly sources: Source[];
+
+    constructor(files: string[], config: TSConfig)
+    {
+        this.sources = files.map(f => new Source(f, config));
+    }
+}
+
 try
 {
     const cfg: Config = new Config();
     const tsc: TSConfig = new TSConfig();
+    const sources: Sources = new Sources(tsc.files, tsc);
+
+    for (let source of sources.sources)
+    {
+        console.log(`${source.file} (${source.key})`);
+
+        TS.forEachChild(source.source, node =>
+        {
+            if (TS.isImportDeclaration(node))
+            {
+                console.log(node.moduleSpecifier)
+            }
+        });
+    }
 
     tsc.convert(cfg.main);
 }
 catch(e)
 {
     console.log(e)
-    process.exit(-1);
+    process.exitCode = -1;
 }
-
-process.exit(0);
 
 function unixify(file: string): string
 {
