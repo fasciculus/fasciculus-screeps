1ee2b016a674c438ee04e953b1b5de5d0f70d259
rhjoerg <rhjoerg@gmail.com>
2024-02-15 14:27:27 +0100

    more array functions

--- source/fasciculus-bot/src/es/array.ts
+++ source/fasciculus-bot/src/es/array.ts
@@ -29,15 +29,70 @@ export class Arrays
         return result;
     }
 
+    //private static groupBy<K, T>(this: Array<T>, toKey: (value: T) => K): Map<K, Array<T>>
+    //{
+    //    const result: Map<K, Array<T>> = new Map();
+
+    //    for (const value of this)
+    //    {
+    //        const key: K = toKey(value);
+
+    //        result.ensure(key, () => new Array()).push(value);
+    //    }
+
+    //    return result;
+    //}
+
+    private static sum<T>(this: Array<T>, toNumber: (value: T) => number): number
+    {
+        var result: number = 0;
+
+        this.forEach(v => { result += toNumber(v); });
+
+        return result;
+    }
+
+    private static defined<T>(values: Array<T | undefined>): Array<T>
+    {
+        var result: Array<T> = new Array<T>();
+
+        for (const value of values)
+        {
+            if (value !== undefined)
+            {
+                result.push(value);
+            }
+        }
+
+        return result;
+    }
+
+    private static flatten<T>(arrays: Array<Array<T>>): Array<T>
+    {
+        const result: Array<T> = new Array();
+
+        arrays.forEach(a => result.append(a));
+
+        return result;
+    }
+
     private static _instanceProperties: any =
         {
             "append": Objects.function(Arrays.append),
             "take": Objects.function(Arrays.take),
             "indexBy": Objects.function(Arrays.indexBy),
+            "sum": Objects.function(Arrays.sum),
         };
+
+    private static _classProperties: any =
+        {
+            "defined": Objects.function(Arrays.defined),
+            "flatten": Objects.function(Arrays.flatten),
+        }
 
     static setup()
     {
         Object.defineProperties(Array.prototype, Arrays._instanceProperties);
+        Object.defineProperties(Array, Arrays._classProperties);
     }
 }
--- source/fasciculus-bot/src/es/types.ts
+++ source/fasciculus-bot/src/es/types.ts
@@ -7,6 +7,15 @@ declare global
         take(count: number): Array<T>;
 
         indexBy<K>(toKey: (value: T) => K): Map<K, T>;
+        groupBy<K>(toKey: (value: T) => K): Map<K, Array<T>>;
+
+        sum(toNumber: (value: T) => number): number;
+    }
+
+    interface ArrayConstructor
+    {
+        defined<T>(array: Array<T | undefined>): Array<T>;
+        flatten<T>(arrays: Array<Array<T>>): Array<T>;
     }
 }
 
