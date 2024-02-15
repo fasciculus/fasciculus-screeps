77b298cf07cdd0bf62bbde54de050a1300fe0508
rhjoerg <rhjoerg@gmail.com>
2024-02-15 14:10:19 +0100

    Objects.keys, Objects.values

--- source/fasciculus-bot/src/es/object.ts
+++ source/fasciculus-bot/src/es/object.ts
@@ -1,6 +1,28 @@
 
 export class Objects
 {
+    static keys(o: {} | undefined | null): Set<string>
+    {
+        if (o === undefined || o === null) return new Set();
+
+        const keys = Object.keys(o);
+
+        if (!keys || !Array.isArray(keys) || keys.length == 0) return new Set();
+
+        return new Set(keys);
+    }
+
+    static values<T>(o: { [s: string]: T; } | ArrayLike<T> | undefined | null): Array<T>
+    {
+        if (o === undefined || o === null) return new Array<T>();
+
+        const values = Object.values(o);
+
+        if (!values || !Array.isArray(values) || values.length == 0) return new Array<T>();
+
+        return values;
+    }
+
     static function(fn: Function): PropertyDescriptor
     {
         const result: PropertyDescriptor =
