8cf3a3204c05cc45f91e3e362e428cdc12a18a4d
rhjoerg <rhjoerg@gmail.com>
2024-02-15 14:21:13 +0100

    added Array.indexBy

--- source/fasciculus-bot/src/es/array.ts
+++ source/fasciculus-bot/src/es/array.ts
@@ -17,10 +17,23 @@ export class Arrays
         return this.slice(0, Math.max(0, Math.min(count, this.length)));
     }
 
+    private static indexBy<K, T>(this: Array<T>, toKey: (value: T) => K): Map<K, T>
+    {
+        const result: Map<K, T> = new Map();
+
+        for (const value of this)
+        {
+            result.set(toKey(value), value);
+        }
+
+        return result;
+    }
+
     private static _instanceProperties: any =
         {
             "append": Objects.function(Arrays.append),
-            "take": Objects.function(Arrays.take)
+            "take": Objects.function(Arrays.take),
+            "indexBy": Objects.function(Arrays.indexBy),
         };
 
     static setup()
--- source/fasciculus-bot/src/es/types.ts
+++ source/fasciculus-bot/src/es/types.ts
@@ -5,6 +5,8 @@ declare global
     {
         append(values: Array<T>): number;
         take(count: number): Array<T>;
+
+        indexBy<K>(toKey: (value: T) => K): Map<K, T>;
     }
 }
 
