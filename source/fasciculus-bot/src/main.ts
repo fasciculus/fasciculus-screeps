216973bc252c6cee6f2f082c831d75e08575a8b0
rhjoerg <rhjoerg@gmail.com>
2024-02-15 14:11:51 +0100

    clear

--- source/fasciculus-bot/src/main.ts
+++ source/fasciculus-bot/src/main.ts
@@ -4,9 +4,4 @@ ES.setup();
 
 export const loop = function ()
 {
-    const a: Array<number> = Array.from([1, 2, 3]);
-    const b: Array<number> = Array.from([4, 5]);
-
-    a.append(b);
-    console.log(a);
 }
