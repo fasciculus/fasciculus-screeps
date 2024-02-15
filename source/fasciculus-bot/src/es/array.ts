77ddf84809594e7381372ed6c2d30bd00f78d2c3
rhjoerg <rhjoerg@gmail.com>
2024-02-15 14:11:40 +0100

    property dscriptor map as "any"

--- source/fasciculus-bot/src/es/array.ts
+++ source/fasciculus-bot/src/es/array.ts
@@ -12,10 +12,16 @@ export class Arrays
         return this.length;
     }
 
-    private static _instanceProperties: PropertyDescriptorMap =
+    private static take<T>(this: Array<T>, count: number): Array<T>
     {
-        "append": Objects.function(Arrays.append)
+        return this.slice(0, Math.max(0, Math.min(count, this.length)));
     }
+
+    private static _instanceProperties: any =
+        {
+            "append": Objects.function(Arrays.append),
+            "take": Objects.function(Arrays.take)
+        };
 
     static setup()
     {
